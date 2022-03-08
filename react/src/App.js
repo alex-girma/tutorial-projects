c01a3200699ede4fb0304ab64eaa8fe5bfbb4c25
Alemayehu Girma <alemayehu.girma@hotmail.com>
2022-03-08 13:48:00 +0100

    work on pets

--- react/src/App.js
+++ react/src/App.js
@@ -1,15 +1,27 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
-import Pet from "./Pet";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
 
 const App = () => {
 	return (
 		<div>
-			<h1 id="my-brand">Adopt Me</h1>
-			<Pet name="Luna" animal="Dog" breed="Havanese" />
-			<Pet name="Pepper" animal="Bird" breed="Cockatiel" />
-			<SearchParams />
+			<Router>
+				<header>
+					<Link to="/">
+						<h1>Adopt Me</h1>
+					</Link>
+				</header>
+				<Switch>
+					<Route path="/details/:id">
+						<Details />
+					</Route>
+					<Route path="/">
+						<SearchParams />
+					</Route>
+				</Switch>
+			</Router>
 		</div>
 	);
 };
---
+++ react/src/Details.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+//import { withRouter } from "react-router-dom";
+
+class Details extends Component {
+	constructor() {
+		super();
+		this.state = { loading: true };
+	}
+	render() {
+		return <h2>Hello</h2>;
+	}
+}
+
+export default Details;
