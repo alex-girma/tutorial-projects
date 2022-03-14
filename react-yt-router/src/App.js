178d5d4b3094442c38a1fd2a287013c8a930c5b2
Alemayehu Girma <alemayehu.girma@hotmail.com>
2022-03-14 20:08:25 +0100

    router

--- react-yt-router/src/App.js
+++ react-yt-router/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./Header";
+import Nav from "./Nav";
+import Footer from "./Footer";
+import Home from "./Home";
+import NewPost from "./NewPost";
+import PostPage from "./PostPage";
+import About from "./About";
+import Missing from "./Missing";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Routes>
+			<div className="App">
+				<Header />
+				<Nav />
+				<Home />
+				<NewPost />
+				<PostPage />
+				<About />
+				<Missing />
+				<Footer />
+			</div>
+		</Routes>
+	);
 }
 
 export default App;
