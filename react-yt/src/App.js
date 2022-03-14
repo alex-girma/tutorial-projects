89fff571a65c84487947240703ee7272addb2cdc
Alemayehu Girma <alemayehu.girma@hotmail.com>
2022-03-14 17:47:23 +0100

    fetch api useeffect

--- react-yt/src/App.js
+++ react-yt/src/App.js
@@ -4,33 +4,80 @@ import AddItems from "./AddItems";
 import Content from "./Content";
 import Footer from "./Footer";
 import { useEffect, useState } from "react"; // importing the react useState hook
+import apiReqeust from "./apiReqeust";
 
 function App() {
-	const [items, setItems] = useState(JSON.parse(localStorage.getItem("shoppinglist")) || []);
+	const API_URL = "http://localhost:3500/items";
+	const [items, setItems] = useState([]);
 	const [newItem, setNewItem] = useState("");
 	const [search, setSearch] = useState("");
+	const [fetchError, setFetchError] = useState(null);
+	const [isLoading, setIsLoading] = useState(true);
 
 	useEffect(() => {
-		localStorage.setItem("shoppinglist", JSON.stringify(items));
-	}, [items]);
+		const fetchItems = async () => {
+			try {
+				const response = await fetch(API_URL);
+				if (!response.ok) throw Error("Did not receive expected data");
+				const listItems = await response.json();
+				setItems(listItems);
+				setFetchError(null);
+			} catch (err) {
+				setFetchError(err.message);
+			} finally {
+				setIsLoading(false);
+			}
+		};
 
-	const addItem = (item) => {
+		fetchItems();
+	}, []);
+
+	const addItem = async (item) => {
 		const id = items.length ? items[items.length - 1].id + 1 : 1;
 		const myNewItem = { id, checked: false, item };
 		const listItems = [...items, myNewItem];
 		setItems(listItems);
+
+		const postOptions = {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(myNewItem),
+		};
+		const result = await apiReqeust(API_URL, postOptions);
+		if (result) setFetchError(result);
 	};
 
-	const handleCheck = (id) => {
+	const handleCheck = async (id) => {
 		const listItems = items.map((item) =>
 			item.id === id ? { ...item, checked: !item.checked } : item
 		);
 		setItems(listItems);
+
+		const myItem = listItems.filter((item) => item.id === id);
+		const updateOptions = {
+			method: "PATCH",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ checked: myItem[0].checked }),
+		};
+		const reqUrl = `${API_URL}/${id}`;
+		const result = await apiReqeust(reqUrl, updateOptions);
+		if (result) setFetchError(result);
 	};
 
-	const handleDelete = (id) => {
+	const handleDelete = async (id) => {
 		const listItems = items.filter((item) => item.id !== id);
 		setItems(listItems);
+
+		const deleteOptions = {
+			method: "DELETE",
+		};
+		const reqUrl = `${API_URL}/${id}`;
+		const result = await apiReqeust(reqUrl, deleteOptions);
+		if (result) setFetchError(result);
 	};
 
 	const handleSubmit = (e) => {
@@ -44,11 +91,17 @@ function App() {
 			<Header title="Grocery List" />
 			<AddItems newItem={newItem} setNewItem={setNewItem} handleSubmit={handleSubmit} />
 			<SearchItem search={search} setSearch={setSearch} />
-			<Content
-				items={items.filter((item) => item.item.toLowerCase().includes(search.toLowerCase()))}
-				handleCheck={handleCheck}
-				handleDelete={handleDelete}
-			/>
+			<main>
+				{isLoading && <p>Loading Items...</p>}
+				{fetchError && <p style={{ color: "red" }}>{`Error: ${fetchError}`}</p>}
+				{!fetchError && !isLoading && (
+					<Content
+						items={items.filter((item) => item.item.toLowerCase().includes(search.toLowerCase()))}
+						handleCheck={handleCheck}
+						handleDelete={handleDelete}
+					/>
+				)}
+			</main>
 			<Footer length={items.length} />
 		</div>
 	);
--- react-yt/src/Content.js
+++ react-yt/src/Content.js
@@ -2,13 +2,13 @@ import ItemList from "./ItemList";
 
 const Content = ({ items, handleCheck, handleDelete }) => {
 	return (
-		<main>
+		<>
 			{items.length ? (
 				<ItemList items={items} handleCheck={handleCheck} handleDelete={handleDelete} />
 			) : (
 				<p style={{ marginTop: "2rem" }}>Your List is empty</p>
 			)}
-		</main>
+		</>
 	);
 };
 
